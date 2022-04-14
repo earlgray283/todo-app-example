ebf59c3911b98f4d3d0d5e9857daf507f5d3babc
earlgray <earlgray283@gmail.com>
2022-04-14 13:25:01 +0900

    fix: InMemoryCache config(make addTypename false)

--- frontend/src/apis/client.ts
+++ frontend/src/apis/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'http://localhost:8080/query',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({ addTypename: false }), // デフォルトだと response に "__typename" field が付加されてしまうので false にする
 });
 
 export default client;
